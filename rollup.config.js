627049e1b96abafdab9fc01fbe7660987deea11a
max <harumaxy@gmail.com>
2020-09-13 23:00:45 +0900

    introduce tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,29 @@
-import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
-import commonjs from '@rollup/plugin-commonjs'
-import svelte from 'rollup-plugin-svelte'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import glob from 'rollup-plugin-glob'
-import config from 'sapper/config/rollup.js'
-import markdown from './src/utils/markdown.js'
-import pkg from './package.json'
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import glob from "rollup-plugin-glob";
+import config from "sapper/config/rollup.js";
+import markdown from "./src/utils/markdown.js";
+import pkg from "./package.json";
+import sveltePreprocess from "svelte-preprocess";
 
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
-const legacy = !!process.env.SAPPER_LEGACY_BUILD
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
-    warning.message.includes('/@sapper/')) ||
-  onwarn(warning)
+  (warning.code === "CIRCULAR_DEPENDENCY" &&
+    warning.message.includes("/@sapper/")) ||
+  onwarn(warning);
+
+const preprocess = sveltePreprocess({
+  postcss: {
+    plugins: [require("postcss-import")(), require("postcss-nested")()],
+  },
+});
 
 export default {
   client: {
@@ -24,8 +31,8 @@ export default {
     output: config.client.output(),
     plugins: [
       replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
       }),
       svelte({
         dev,
@@ -38,21 +45,21 @@ export default {
       glob(),
       legacy &&
         babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          babelHelpers: 'runtime',
-          exclude: ['node_modules/@babel/**'],
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          babelHelpers: "runtime",
+          exclude: ["node_modules/@babel/**"],
           presets: [
             [
-              '@babel/preset-env',
+              "@babel/preset-env",
               {
-                targets: '> 0.25%, not dead',
+                targets: "> 0.25%, not dead",
               },
             ],
           ],
           plugins: [
-            '@babel/plugin-syntax-dynamic-import',
+            "@babel/plugin-syntax-dynamic-import",
             [
-              '@babel/plugin-transform-runtime',
+              "@babel/plugin-transform-runtime",
               {
                 useESModules: true,
               },
@@ -74,11 +81,12 @@ export default {
     output: config.server.output(),
     plugins: [
       replace({
-        'process.browser': false,
-        'process.env.NODE_ENV': JSON.stringify(mode),
+        "process.browser": false,
+        "process.env.NODE_ENV": JSON.stringify(mode),
       }),
       svelte({
-        generate: 'ssr',
+        preprocess,
+        generate: "ssr",
         dev,
       }),
       resolve(),
@@ -87,8 +95,8 @@ export default {
       glob(),
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require('module').builtinModules ||
-        Object.keys(process.binding('natives'))
+      require("module").builtinModules ||
+        Object.keys(process.binding("natives"))
     ),
 
     onwarn,
@@ -100,8 +108,8 @@ export default {
     plugins: [
       resolve(),
       replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
       }),
       commonjs(),
       !dev && terser(),
@@ -109,4 +117,4 @@ export default {
 
     onwarn,
   },
-}
+};
