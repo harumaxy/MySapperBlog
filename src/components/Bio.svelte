6e64f2da4f5710cb9ca29246f8c8e4e2ab193ac2
max <harumaxy@gmail.com>
2020-09-15 01:56:54 +0900

    update bio

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -19,11 +19,11 @@
 </style>
 
 <div>
-  <img src="profile-pic.png" alt="Maxi Ferreira">
+  <img src="avator.jpeg" alt="Max" />
   <p>
-    Hi, I'm <strong>Maxi</strong>. I'm a software engineer and data scientist from Córdoba, Argentina.
-    You can follow me on <a href="https://www.twitter.com/Charca" target="_blank">Twitter</a>,
-    see some of my work on <a href="https://www.github.com/Charca" target="_blank">GitHub</a>,
-    or read more about me on <a href="https://www.maxiferreira.com" target="_blank">my website</a>.
+    Max <br />
+    <a href="https://www.twitter.com/maxy8653" target="_blank">Twitter</a>, <a
+      href="https://www.github.com/harumaxy"
+      target="_blank">GitHub</a>
   </p>
 </div>
